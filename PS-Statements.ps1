2f14348395a28ccf3f47bbc532767c96479254be
Anto <55436023+ToniCoding@users.noreply.github.com>
2022-09-21 17:55:34 +0200

    Add files via upload

---
+++ PS-Statements.ps1
@@ -0,0 +1,21 @@
+﻿# Statements help us control the flow of our program depending on certain circunstances.
+# Duct tape stock stored in a variable.
+$ductTape = 30
+
+# Flow control.
+if($ductTape -ge 15 -or $ductTape -gt 15) {
+    Write-Host "We still have stock on duct tape."
+} elseif($ductTape -eq 0) {
+    Write-Host "We ran out of stock on duct tape."
+} elseif($ductTape -le 15) {
+    Write-Host "We are running out on duct tape."
+} else {
+    Write-Host "Duct tape stock is unknown."
+}
+
+# LEGEND:
+# -ge = Greater (or) Equal.
+# -gt = Greater Than.
+# -eq = Equal (to).
+# -le = Less (or) Equal.
+# -lt = Less Than.
---
+++ PS-Variables.ps1
@@ -0,0 +1,22 @@
+﻿# In PowerShell you can declare variables by using $ followed by the name of the variable.
+# This variable stores someones age.
+# Variable stores: Integer
+$age = 20
+
+# This variable stores someones password.
+# Variable stores: String
+$passwd = "risingMoon_1234"
+
+# This variable stores someones name and last name with spaces(NOT RECOMMENDED!).
+# Variable stores: String
+${Full Name} = "Robert Miles"
+
+# This variable stores if it's someone is administrator or not.
+# Variable stores: Boolean
+$isAdmin = $false
+$isAdminUser = $true
+
+# To print the variables just use the Write-Host command followed by the $var.
+Write-Host $age
+Write-Host $passwd
+Write-Host ${Full Name}
